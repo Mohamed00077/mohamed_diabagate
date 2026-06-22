e1811115c85d5657105cd97736649c3518d63320
Momo Diabagate <momodiabagate71@gmail.com>
2026-06-22 10:54:46 +0000

    add logo

--- my-porfolio/app/page.tsx
+++ my-porfolio/app/page.tsx
@@ -234,12 +234,14 @@ export default function Home() {
       <nav className="fixed top-0 w-full z-50 hologram">
         <div className="max-w-7xl mx-auto px-6 lg:px-8">
           <div className="flex items-center justify-between h-20">
-            <div
-              className="orbitron text-2xl font-bold neon-text cursor-pointer"
-              onMouseEnter={handleLogoHover}
-            >
-              &lt;MOMO/&gt;
-            </div>
+            <Image
+  src="/icon.png"
+  alt="Momo logo"
+  width={60}
+  height={35}
+   className="object-contain rounded-full" 
+  style={{ filter: "drop-shadow(0 0 8px #00ffff)" }}
+/>
 
             <div className="hidden md:flex space-x-8">
               {navLinks.map(({ id, label }) => (
